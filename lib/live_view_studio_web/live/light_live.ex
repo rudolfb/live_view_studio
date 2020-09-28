29a0aaf092d76292aa84724834d2f49ab7659b7e
Rudolf Bargholz <rudolf@bargholz.ch>
2020-09-28 17:44:34 +0200

    Almost finished Still need to do the "Find a Flight" excercise

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,12 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :brightness, 10)
+    socket =
+      assign(socket,
+        brightness: 10,
+        temp: 3000
+      )
+
     {:ok, socket}
   end
 
@@ -11,7 +16,7 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style="width: <%= @brightness %>%">
+        <span style="width: <%= @brightness %>%; background-color: <%= temp_color(@temp) %>">
           <%= @brightness %>%
         </span>
       </div>
@@ -31,6 +36,18 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="on">
         <img src="images/light-on.svg">
       </button>
+
+      <form phx-change="change-temp">
+        <input type="radio" id="3000" name="temp" value="3000" <%= if 3000 == @temp, do: "checked" %> />
+        <label for="3000">3000</label>
+
+        <input type="radio" id="4000" name="temp" value="4000" <%= if 4000 == @temp, do: "checked" %> />
+        <label for="4000">4000</label>
+
+        <input type="radio" id="5000" name="temp" value="5000" <%= if 5000 == @temp, do: "checked" %> />
+        <label for="5000">5000</label>
+      </form>
+
     </div>
     """
   end
@@ -54,4 +71,14 @@ defmodule LiveViewStudioWeb.LightLive do
     socket = assign(socket, :brightness, 0)
     {:noreply, socket}
   end
+
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    temp = String.to_integer(temp)
+    socket = assign(socket, temp: temp)
+    {:noreply, socket}
+  end
+
+  defp temp_color(3000), do: "#F1C40D"
+  defp temp_color(4000), do: "#FEFF66"
+  defp temp_color(5000), do: "#99CCFF"
 end
